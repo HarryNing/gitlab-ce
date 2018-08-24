514662521e30744310211bb053001a7d7c6f8932
Robert Schilling <rschilling@student.tugraz.at>
2018-08-24 09:35:27 +0200

    Add specs

--- lib/api/entities.rb
+++ lib/api/entities.rb
@@ -1017,7 +1017,7 @@ module API
       end
 
       expose :subscribed do |label, options|
-        label.subscribed?(options[:current_user], options[:project])
+        label.subscribed?(options[:current_user], options[:parent])
       end
     end
 
